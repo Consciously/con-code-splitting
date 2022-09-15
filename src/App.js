1d51e8a754a26160d6470cc8b291f787a2faf1fc
Stefan Ihle <stefanihle@gmx.net>
2022-09-15 10:59:34 +0200

    add custom routing functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Page1 from './components/Page1';
 import Page2 from './components/Page2';
 import Page3 from './components/Page3';
 
+const defaultRoute = {
+	route: 'page1',
+};
+
 function App() {
+	const [route, setRoute] = useState(defaultRoute);
+
+	const onRouteChange = route => {
+		setRoute({ route });
+	};
+
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-			<Page1 />
-			<Page2 />
-			<Page3 />
+			{route.route === 'page1' ? (
+				<Page1 onRouteChange={onRouteChange} />
+			) : route.route === 'page2' ? (
+				<Page2 onRouteChange={onRouteChange} />
+			) : route.route === 'page3' ? (
+				<Page3 onRouteChange={onRouteChange} />
+			) : null}
 		</div>
 	);
 }
