6d05b896bfe4a71230fe054e8dbde1749ef6ace4
Stefan Ihle <stefanihle@gmx.net>
2022-09-15 16:58:46 +0200

    currrent state without codesplitting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import { useState } from 'react';
 import './App.css';
-import withAsyncComponent from './components/withAsyncComponent';
 import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
 
 const defaultRoute = {
 	route: 'page1',
-	component: null,
 };
 
 function App() {
 	const [route, setRoute] = useState(defaultRoute);
 
 	const onRouteChange = async route => {
+		// Without code splitting
 		setRoute({ ...route, route: route });
+
+		// With code splitting
+
+		// if (route === 'page1') {
+		// 	setRoute({ ...route, route: route });
+		// } else if (route === 'page2') {
+		// 	import('./components/Page2').then(Page2 => {
+		// 		setRoute({ ...route, component: Page2.default });
+		// 	});
+		// } else if (route === 'page3') {
+		// 	import('./components/Page3').then(Page3 => {
+		// 		setRoute({ ...route, component: Page3.default });
+		// 	});
+		// }
 	};
 
 	let content;
@@ -20,14 +35,21 @@ function App() {
 	if (route.route === 'page1') {
 		content = <Page1 onRouteChange={onRouteChange} />;
 	} else if (route.route === 'page2') {
-		const AsyncPage2 = withAsyncComponent(import('./components/Page2'));
-		content = <AsyncPage2 onRouteChange={onRouteChange} />;
+		content = <Page2 onRouteChange={onRouteChange} />;
 	} else if (route.route === 'page3') {
-		const AsyncPage3 = withAsyncComponent(import('./components/Page3'));
-		content = <AsyncPage3 onRouteChange={onRouteChange} />;
+		content = <Page3 onRouteChange={onRouteChange} />;
 	}
 
-	return <div className='App'>{content}</div>;
+	return (
+		<div className='App'>
+			{content}
+			{/* {route.route === 'page1' ? (
+				<Page1 onRouteChange={onRouteChange} />
+			) : (
+				<route.component onRouteChange={onRouteChange} />
+			)} */}
+		</div>
+	);
 }
 
 export default App;
