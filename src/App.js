3738c5e175c7fb995dc31c3abb6c8990e83b9fc1
Stefan Ihle <stefanihle@gmx.net>
2022-09-15 17:21:21 +0200

    codeSplitting without hoc

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 import './App.css';
 import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import Page3 from './components/Page3';
 
 const defaultRoute = {
 	route: 'page1',
@@ -12,20 +10,28 @@ function App() {
 	const [route, setRoute] = useState(defaultRoute);
 
 	const onRouteChange = async route => {
-		setRoute({ ...route, route: route });
+		if (route === 'page1') {
+			setRoute({ ...route, route: route });
+		} else if (route === 'page2') {
+			import('./components/Page2').then(Page2 => {
+				setRoute({ ...route, component: Page2.default });
+			});
+		} else if (route === 'page3') {
+			import('./components/Page3').then(Page3 => {
+				setRoute({ ...route, component: Page3.default });
+			});
+		}
 	};
 
-	let content;
-
-	if (route.route === 'page1') {
-		content = <Page1 onRouteChange={onRouteChange} />;
-	} else if (route.route === 'page2') {
-		content = <Page2 onRouteChange={onRouteChange} />;
-	} else if (route.route === 'page3') {
-		content = <Page3 onRouteChange={onRouteChange} />;
-	}
-
-	return <div className='App'>{content}</div>;
+	return (
+		<div className='App'>
+			{route.route === 'page1' ? (
+				<Page1 onRouteChange={onRouteChange} />
+			) : (
+				<route.component onRouteChange={onRouteChange} />
+			)}
+		</div>
+	);
 }
 
 export default App;
